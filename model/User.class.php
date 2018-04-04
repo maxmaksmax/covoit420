8668737ee0fc3d190b5423bd92b22a31943b22d2
Maxime <maxime.i.lehmann@gmail.com>
2018-04-04 17:23:22 +0200

    modif sql requetes

--- model/User.class.php
+++ model/User.class.php
@@ -3,7 +3,7 @@
 class User extends Model {
 	
 	public static function isEmailUsed($email){
-		$user = Model::executeRequest('select count(*) from utilisateur where utilisateur.email = :email', array(':email' => $email));
+		$user = Model::executeRequest('CountUsersWithEmail', array(':email' => $email));
 		foreach($user as $u){
 			if($u['count(*)'] == 0){
 				return false;
@@ -16,21 +16,18 @@ class User extends Model {
 	
 	public static function create($email, $prenom, $nom, $admin, $telephone, $password){ 
 	
-		$requete = "INSERT INTO utilisateur(id_user, password, email, prenom_user, nom_user, est_admin, telephone) VALUES (:id, :password, :email,  :prenom_user, :nom_user,
-				:est_admin, :telephone);";
 		if (self::isEmailUsed($email)){
 			echo 'Email déjà utilisé !';
 			exit();
 		}
 		else{
-			$user = Model::executeRequest($requete, array('id' => rand(10000,10100), 'password' => $password, 'email' => $email, 'nom_user' => $nom, 'prenom_user' => $prenom, 'est_admin' => $admin, 'telephone' => $telephone));
+			$user = Model::executeRequest('CreateUser', array('id' => rand(10000,10100), 'password' => $password, 'email' => $email, 'nom_user' => $nom, 'prenom_user' => $prenom, 'est_admin' => $admin, 'telephone' => $telephone));
 		}
 		return $user;
 	}
 	
 	public static function getPassword($email){
-		$requete = "SELECT password FROM utilisateur WHERE utilisateur.email = :email";
-		$stmt = Model::executeRequest($requete, array('email' => $email)) -> fetch()[0];
+		$stmt = Model::executeRequest('PrintPassword', array('email' => $email)) -> fetch()[0];
 		if($stmt == null){
 			echo "Cet utilisateur n'existe pas !";
 		}
@@ -40,8 +37,7 @@ class User extends Model {
 	}
 	
 	public static function getNom($email){
-		$requete = "SELECT nom FROM utilisateur WHERE utilisateur.email = :email";
-		$stmt = Model::executeRequest($requete, array('email' => $email)) -> fetch()[0];
+		$stmt = Model::executeRequest('PrintNom', array('email' => $email)) -> fetch()[0];
 		if($stmt == null){
 			echo "Cet utilisateur n'existe pas !";
 		}
@@ -51,8 +47,7 @@ class User extends Model {
 	}	
 
 	public static function getPrenom($email){
-		$requete = "SELECT prenom FROM utilisateur WHERE utilisateur.email = :email";
-		$stmt = Model::executeRequest($requete, array('email' => $email)) -> fetch()[0];
+		$stmt = Model::executeRequest('PrintPrenom', array('email' => $email)) -> fetch()[0];
 		if($stmt == null){
 			echo "Cet utilisateur n'existe pas !";
 		}
@@ -62,8 +57,7 @@ class User extends Model {
 	}
 	
 	public static function getTelephone($email){
-		$requete = "SELECT telephone FROM utilisateur WHERE utilisateur.email = :email";
-		$stmt = Model::executeRequest($requete, array('email' => $email)) -> fetch()[0];
+		$stmt = Model::executeRequest('PrintTelephone', array('email' => $email)) -> fetch()[0];
 		if($stmt == null){
 			echo "Cet utilisateur n'existe pas !";
 		}
--- sql/User.sql.php
+++ sql/User.sql.php
@@ -3,14 +3,13 @@
 	User::addSqlRequest('CreateUser', "INSERT INTO `utilisateur`(`login`, `nom_user`, `prenom_user`, `est_admin`, `telephone`, `email`, `password`) 
 	VALUES (:login, :nom_user, :prenom_user, :est_admin, :telephone, :email, :password);");
 			
-	User::addSqlRequest('CountUsersWithLogin', 'select count(*) from utilisateur where utilisateur.login = :login');
+	User::addSqlRequest('CountUsersWithLogin', 'select count(*) from utilisateur where utilisateur.email = :email');
 	
 	
-	User::addSqlRequest('PrintPassword', "SELECT password FROM utilisateur WHERE utilisateur.login = :login");
-	User::addSqlRequest('PrintEmail', "SELECT email FROM utilisateur WHERE utilisateur.login = :login");
-	User::addSqlRequest('PrintTelephone', "SELECT telephone FROM utilisateur WHERE utilisateur.login = :login");
-	User::addSqlRequest('PrintName', "SELECT nom_user FROM utilisateur WHERE utilisateur.login = :login");
-	User::addSqlRequest('PrintPrenom', "SELECT prenom_user FROM utilisateur WHERE utilisateur.login = :login");
+	User::addSqlRequest('PrintPassword', "SELECT password FROM utilisateur WHERE utilisateur.email = :email");
+	User::addSqlRequest('PrintTelephone', "SELECT telephone FROM utilisateur WHERE utilisateur.email = :email");
+	User::addSqlRequest('PrintNom', "SELECT nom FROM utilisateur WHERE utilisateur.email = :email");
+	User::addSqlRequest('PrintPrenom', "SELECT prenom FROM utilisateur WHERE utilisateur.email = :email");
    
 
 
