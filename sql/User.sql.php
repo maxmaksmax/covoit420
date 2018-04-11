3a12148b92498c82941873478755d3eb3903b0d8
Maxime Servillat <maxime.servillat@etu.imt-lille-douai.fr>
2018-04-11 10:39:16 +0200

    model+sql

--- sql/User.sql.php
+++ sql/User.sql.php
@@ -1,16 +1,16 @@
 <?php
 
-	User::addSqlRequest('CreateUser', "INSERT INTO `utilisateur`(`email`, `nom_user`, `prenom_user`, `est_admin`, `telephone`,  `password`) 
+	User::addSqlRequest('CreateUser', "INSERT INTO `utilisateur`(`email`, `nom_user`, `prenom_user`, `est_admin`, `telephone`,  `password`)
 	VALUES (:email, :nom_user, :prenom_user, :est_admin, :telephone, :password);");
-			
+
 	User::addSqlRequest('CountUsersWithLogin', 'select count(*) from utilisateur where utilisateur.email = :email');
-	
-	
+
+
 	User::addSqlRequest('PrintPassword', "SELECT password FROM utilisateur WHERE utilisateur.email = :email");
 	User::addSqlRequest('PrintTelephone', "SELECT telephone FROM utilisateur WHERE utilisateur.email = :email");
 	User::addSqlRequest('PrintNom', "SELECT nom FROM utilisateur WHERE utilisateur.email = :email");
 	User::addSqlRequest('PrintPrenom', "SELECT prenom FROM utilisateur WHERE utilisateur.email = :email");
-   
+
 
 
 ?>
