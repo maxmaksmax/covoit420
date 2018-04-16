1bf126c9b0a5e3873a17e733fe1e9f13947ec593
Maxime <maxime.i.lehmann@gmail.com>
2018-04-16 18:28:57 +0200

    template de création de trajet

--- templates/creationTrajetTemplate.php
+++ templates/creationTrajetTemplate.php
@@ -1,40 +1,64 @@
-<section>
 
-<section >
-      <div >
-        <div>
-          <div >
-            <h2>Proposer un trajet</h2>
+<section class="content-section" id="portfolio">
+ 
+ <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Proposer un trajet</h2>
           </div>
         </div>
-        <div>
-          <div>
-            <form action="index.php?c=user&a=creationTrajet" method="post">
+        <div class="row">
+          <div class="col-lg-12">
+            <form action="index.php?c=user&a=creationTrajet" method="post"  >
               <div class="row">
-                <div class="col-md-6">
-                  <div class="form-group">
-                    <input class="form-control" id="date" name="date" type="date" placeholder="Date" required data-validation-required-message="Sélectionner une date">
+			  
+			<div class="col-md-6">
+                 <div class="form-group">
+                    <input class="form-control" id="lieu_depart" name="lieu_depart" type="text" placeholder="Lieu de départ" required data-validation-required-message="Selectionner un lieu de départ">
                     <p class="help-block text-danger"></p>
                   </div>
-                  <div class="form-group">
-                    <input class="form-control" id="lieuDepart" name="lieuDepart" type="text" placeholder="Lieu Départ" required data-validation-required-message="Sélectionner un lieu de départ">
+				  <div class="form-group">
+                    <input class="form-control" id="lieu_arrivee" name="lieu_arrivee" type="text" placeholder="Lieu d'arrivée" required data-validation-required-message="Selectionner un lieu de départ">
                     <p class="help-block text-danger"></p>
                   </div>
+				  <div class="form-group">
+                    <input class="form-control" id="nombre_places" name="nombre_places" type="text" placeholder="Nombre de places disponibles" required data-validation-required-message="Selectionner un nombre de places disponibles">
+                    <p class="help-block text-danger"></p>
                 </div>
-                <div class="col-md-6">
-                   <div class="form-group">
-                    <input class="form-control" id="heure" name="heure" type="time" placeholder="Heure" required data-validation-required-message="Sélectionner une heure">
+			</div>
+			
+            <div class="col-md-6">
+				<div class="form-group">
+                    <input class="form-control" id="date" name="date" type="date" placeholder="Date" required data-validation-required-message="Date">
                     <p class="help-block text-danger"></p>
-                  </div>
-                  <div class="form-group">
-                    <input class="form-control" id="lieuArrivee" name="lieuArrivee" type="text" placeholder="Lieu d'arrivée" required data-validation-required-message="Sélectionner un lieu de départ">
-                    <p class="help-block text-danger"></p>
-                  </div>
                 </div>
+				<div class="form-group">
+                    <input class="form-control" id="heure_depart" name="heure_depart" type="text" placeholder="Heure de départ au format hh:mm" required data-validation-required-message="Heure de départ">
+                    <p class="help-block text-danger"></p>
+                </div>
+				<div class="form-group">
+                    <input class="form-control" id="heure_arrivee" name="heure_arrivee" type="text" placeholder="Heure d'arrivée au format hh:mm" required data-validation-required-message="Heure d'arrivée">
+                    <p class="help-block text-danger"></p>
+                </div>              
+				<div class="form-group">
+					<select name="voiture">
+						<?php for($i=0; $i < sizeof($reponse); $i++){ ?>
+							<option value="<?php echo $reponse[$i][0]?>";>
+							<?php echo $reponse[$i][0]; ?> </option>
+						<?php
+							} 
+						?>
+					</select>
+                    <p class="help-block text-danger"></p>
+                </div>
+			</div>
+			
                 <div class="clearfix"></div>
                 <div class="col-lg-12 text-center">
+				
                   <div id="success"></div>
-                  <input type="submit" value="Proposer" name="boutonProposer" class="btn btn-primary btn-xl text-uppercase"></input>
+                  <input type="submit" value="Créer un trajet" name="creationTrajet" class="btn btn-primary btn-xl text-uppercase"></input>
                 </div>
               </div>
             </form>
@@ -43,3 +67,4 @@
       </div>
     </section>
 </section>
+	
