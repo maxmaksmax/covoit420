0b7dc2f64d8200cc4808fc9ab2893aa4b00f7a22
Maxime <maxime.i.lehmann@gmail.com>
2018-04-16 20:50:42 +0200

    template compte

--- templates/compteTemplate.php
+++ templates/compteTemplate.php
@@ -1,9 +1,58 @@
-<h2> Compte</h2>
 
-<div class="row">
-
-compte
-
-</div>
-
+<section class="content-section" id="portfolio">
+      <div class="container">
+        <div class="content-section-heading text-center">
+          <h3 class="text-secondary mb-0">Compte</h3>
+          <h2 class="mb-5">Gérer mes Trajets</h2>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="index.php?c=user&a=rechercheTrajet">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Rechercher un trajet</h2>
+                  <p class="mb-0">Pas de voiture? Trouve quelqu'un pour te conduire!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="BootStrap/Portfolio/img/portfolio-1.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="index.php?c=user&a=creationTrajet">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Proposer un trajet</h2>
+                  <p class="mb-0">T'as de la place dans ta voiture? Trouve des compagnons!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="BootStrap/Portfolio/img/portfolio-2.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="index.php?c=user&a=historiqueTrajets"> 
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Historique des trajets</h2>
+                  <p class="mb-0">Jette un coup d'oeil à tes précédents voyages!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="BootStrap/Portfolio/img/portfolio-3.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-lg-6">
+            <a class="portfolio-item" href="index.php?c=user&a=statistiques">
+              <span class="caption">
+                <span class="caption-content">
+                  <h2>Statistiques</h2>
+                  <p class="mb-0">Analyse ton profil de covoitureur!</p>
+                </span>
+              </span>
+              <img class="img-fluid" src="BootStrap/Portfolio/img/portfolio-4.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+	
+</body>
 
