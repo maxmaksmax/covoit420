ae13b9752f0677371286a4e54676fdddbf0d328e
Maxime Servillat <maxime.servillat@etu.imt-lille-douai.fr>
2018-04-16 11:37:41 +0200

    divers apparences

--- templates/indexTemplate.php
+++ templates/indexTemplate.php
@@ -1,5 +1,5 @@
 <!--- banner --->
-<div class="content text-center" id='accueil'>
+<div class="text-center" id='accueil'>
   <h1>Plus d'excuse pour ne pas aller en cours !</h1>
   <h2>IMT Express c'est l'assurance de se déplacer au sec !</h2>
 
--- view/UserView.class.php
+++ view/UserView.class.php
@@ -1,15 +1,16 @@
 <?php
 
 class UserView extends View {
-	
+
 	public function __construct($controller, $templateName, $args = array()) {
 		parent::__construct($controller, $templateName, $args = array());
-		$this->templateNames['head'] = 'head'; 
+		$this->templateNames['head'] = 'head';
 		$this->templateNames['menu'] = 'menuUser';
-	} 
-   
+	}
+
 	public function render(){
 		$this->loadTemplate($this->templateNames['head'], $this->args);
+		$this->loadTemplate($this->templateNames['top'], $this->args);
 		$this->loadTemplate($this->templateNames['menu'], $this->args);
 		$this->loadTemplate($this->templateNames['content'], $this->args);
 		$this->loadTemplate($this->templateNames['foot'], $this->args);
