91c59b03b34fbe3a8ab8a9974aefb45f00bca77c
Maxime <maxime.i.lehmann@gmail.com>
2018-04-17 21:28:47 +0200

    h

--- templates/indexTemplate.php
+++ templates/indexTemplate.php
@@ -3,18 +3,8 @@
   <h1>Plus d'excuse pour ne pas aller en cours !</h1>
   <h2>IMT Express c'est l'assurance de se déplacer au sec !</h2>
 
-
-  <ul>
-     <li <?php
-  	  	if($controller->getActionName()=='defaultAction')
-  			echo 'class="active"';
-  	   ?>><a href="index.php?c=user&amp;a=index">Propose un covoiturage</a></li>
-
-     <li <?php
-  	  	if($controller->getActionName()=='creationTrajet')
-  			echo 'class="active"';
-  	   ?>><a href="index.php?c=user&amp;a=creationTrajet">Recherche un covoiturage</a></li>
-  </ul>
+  
+  
   <?php echo $_SESSION['nom'].$_SESSION['prenom'].$_SESSION['id_user']; ?>
 </div>
 <!--- banner --->
