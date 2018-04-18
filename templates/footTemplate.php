7019324d4314e04faaff9c9f510ea4614628fb71
Maxime <maxime.i.lehmann@gmail.com>
2018-04-19 00:35:59 +0200

    j

--- templates/footTemplate.php
+++ templates/footTemplate.php
@@ -1,7 +1,72 @@
 <!---start-bottom-footer---->
+
+
+
+<div class="footer">
+				<div class="top-footer">
+					<div class="container">
+						<div class="top-footer-grids">
+							<div class="top-footer-grid">
+								<h3>Contact us</h3>
+								<ul class="address">
+									<li><span class="map-pin"> </span><label>55 Main St.Toronto, ON M1H 3A5</label></li>
+									<li><span class="mob"> </span>(416) 555-5252</li>
+									<li><span class="msg"> </span><a href="mailto:yourname@yoursite.com">hello@moto.com</a></li>
+								</ul>
+							</div>
+							<div class="top-footer-grid">
+								<h3>Latest posts</h3>
+								<ul class="latest-post">
+									<li><a href="#"><span class="icon1"> </span> Made With Love In Toronto</a> </li>
+									<li><a href="#"><span class="icon2"> </span> Startup News &amp; Emerging Tech</a> </li>
+									<li><a href="#"><span class="icon3"> </span> Bitcoin Will Soon Rule The World</a> </li>
+									<li><a href="#"><span class="icon4"> </span> Wearable Technology On The Rise</a> </li>
+									<li><a href="#"><span class="icon5"> </span> Learn Web Design In <label>30 Days!</label></a> </li>
+								</ul>
+							</div>
+							<div class="top-footer-grid">
+								<h3>Latest tweets</h3>
+								<div class="tweet-box">
+									<div class="tweet-box-icon">
+										<span> </span>
+									</div>
+									<div class="tweet-box-info">
+										<p>Confucius: Life is really simple, but we insist on making it complicated. <span><a href="#">#famousquotes</a></span><label><a href="#"> 8 mins ago</a></label></p>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="tweet-box">
+									<div class="tweet-box-icon">
+										<span> </span>
+									</div>
+									<div class="tweet-box-info">
+										<p>Confucius: Life is really simple, but we insist on making it complicated. <span><a href="#">#famousquotes</a></span><label><a href="#"> 8 mins ago</a></label></p>
+									</div>
+									<div class="clear"> </div>
+								</div>
+							</div>
+							<div class="clear"> </div>
+						</div>
+					</div>
+				</div>
+
+				<!---start-bottom-footer---->
+				<div class="bottom-footer">
+					<div class="container">
+						<div class="bottom-footer-left">
+							<p>&#169; Copyright 2014 Template by <a href="http://w3layouts.com/">W3layouts</a></p>
+						</div>
+						<div class="clear"> </div>
+					</div>
+				</div>
+				<!---//End-bottom-footer---->
+<!--
 <div class="bottom-footer">
   Ce site vous est fourni par Maxime Lehmann et Maxime Servillat
 </div>
+
+
+
 <!---//End-bottom-footer---->
 
 </div>
