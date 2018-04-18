6c6c9d697fa3961abc9687b4b2ff6d5f82a50219
Maxime <maxime.i.lehmann@gmail.com>
2018-04-18 23:18:26 +0200

    n

--- templates/indexTemplate.php
+++ templates/indexTemplate.php
@@ -1,7 +1,7 @@
 <!--- banner --->
 <div class="text-center" id='accueil'>
-  <h1>Plus d'excuse pour ne pas aller en cours !</h1>
-  <h2>IMT Express c'est l'assurance de se déplacer au sec !</h2>
+  <h1>Vous n'avez plus d'excuse pour ne pas aller en cours.</h1>
+  <h2>IMT Express</h2>
 
 </div>
 <!--- banner --->
