4b0a247c2607e13515d679ec79f7b8be8745e95b
Maxime <maxime.i.lehmann@gmail.com>
2018-04-19 15:38:08 +0200

    o

--- templates/validateRechercheTrajetTemplate.php
+++ templates/validateRechercheTrajetTemplate.php
@@ -1,6 +1,8 @@
 <?php
 	$trajets = User::showMesFutursTrajets($_SESSION['id_user']);
 	$trajetsRecherchés = $this->getArg('trajetsRecherchés');
+	//$trajetsR = User::showTrajet($_SESSION['id_user']);
+	
 
 ?>
 
@@ -16,27 +18,28 @@
 			<div class="col-lg-12">
 				<div class="container">
 				<?php
-					$nbTrajets = sizeof($trajets);
+					$nbTrajets = sizeof($trajetsRecherchés);
 					for ($i=0; $i<$nbTrajets; $i++){ ?>
 						<form action="index.php?c=user&a=validateRechercheTrajet" method="post" >
 						<ul class="list-inline mb-2" style="text-align: center;">
 							<li class="list-inline-item">
-								<p class="text" name="lieu_depart"><?php print_r($trajets[$i]['lieu_depart']);?></p>
+								<p class="text" name="lieu_depart"><?php print_r($trajetsRecherchés[$i]['lieu_depart']);?></p>
 							</li>
 							<li class="list-inline-item">
-								<p class="text" name="lieu_arrivee"><?php print_r($trajets[$i]['lieu_arrivee']);?></p>
+								<p class="text" name="lieu_arrivee"><?php print_r($trajetsRecherchés[$i]['lieu_arrivee']);?></p>
 							</li>
 							<li class="list-inline-item">
-								<p class="text" name="heure_depart"><?php print_r($trajets[$i]['heure_depart']);?></p>
+								<p class="text" name="heure_depart"><?php print_r($trajetsRecherchés[$i]['heure_depart']);?></p>
 							</li>
 							<li class="list-inline-item">
-								<p class="text" name="nombre_places"><?php print_r($trajets[$i]['nombre_places']);?></p>
+								<p class="text" name="nombre_places"><?php print_r($trajetsRecherchés[$i]['nombre_places']);?></p>
 							</li>
 							<li class="list-inline-item"
 								<?php
-									if($trajets[$i]['id_user'] == $_SESSION['id_user']){
+									if($trajetsRecherchés[$i]['id_user'] == $_SESSION['id_user']){
 										echo("style=\"visibility:hidden\" ");
 									}
+									// A MODIFIER AVEC UNE FONCTION QUI VERIFIE SI LE USER EST PARTICIPANT, ET NON CREATEUR
 								?>
 							>
 								<input type="submit" value="M'inscrire" name="boutonInscription" class="btn btn-primary btn-xl text-uppercase"></input>
