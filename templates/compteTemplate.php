6894af829eaba16499c6d18dc5c09c53221c88de
Maxime Servillat <maxime.servillat@etu.imt-lille-douai.fr>
2018-04-19 17:22:13 +0200

    blu

--- templates/compteTemplate.php
+++ templates/compteTemplate.php
@@ -18,37 +18,31 @@
               <div class="row">
                 <div class="col-md-6" >
                   <div class="form-group">
-										<label class="control-label col-sm-3" for="nom_user">Nom</label>
                    <input class="form-control champ-non-modifiable" name="nom_user" type="text" value= '<?php print_r($nom);?>'  disabled="disabled">
                     <p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-										<label class="control-label col-sm-3" for="prenom_user">Prénom</label>
                    <input class="form-control champ-non-modifiable" name="prenom_user" type="text" value= '<?php print_r($prenom);?>'  disabled="disabled">
 				   <p class="help-block text-danger"></p>
                   </div>
                 </div>
                 <div class="col-md-6">
                    <div class="form-group" >
-										 <label class="control-label col-sm-3" for="email">Email</label>
                     <input class="form-control champ-non-modifiable" name="email" type="email" value= '<?php print_r($email);?>'  disabled="disabled">
 					<p class="help-block text-danger"></p>
                   </div>
                   <div class="form-group">
-										<label class="control-label col-sm-3" for="telephone">Téléphone</label>
                     <input class="form-control" name="telephone" type="text" value= '<?php print_r($telephone);?>' required data-validation-required-message="Entrez un téléphone">
 					<p class="help-block text-danger"></p>
                   </div>
                 </div>
 								<div class="col-md-6">
                    <div class="form-group">
-										 <label class="control-label col-sm-3" for="site">Site</label>
-                    <input class="form-control" name="site" type="text" value= '<?php print_r($site);?>' required data-validation-required-message="Entrez un site">
+                    <input class="form-control" name="site" type="text" placeholder="Site : "value= '<?php print_r($site);?>' required data-validation-required-message="Entrez un site">
 					<p class="help-block text-danger"></p>
                   </div>
 
                   <div class="form-group">
-										<label class="control-label col-sm-3" for="fonction">Fonction</label>
                     <input class="form-control" name="fonction" type="text" value= '<?php print_r($fonction);?>' required data-validation-required-message="Entrez une fonction">
 					<p class="help-block text-danger"></p>
                   </div>
@@ -56,22 +50,22 @@
 								<br>
 								<div class="col-md-6">
 									<div class="form-group" >
-										<label class="control-label col-sm-3" for="modele">Modèle de voiture</label>
-	                 <input class="col-sm-3 form-control champ-non-modifiable" name="modele" type="text" value= '<?php print_r($voitures[0][1]);?>'  disabled="disabled">
+										<p>Voiture</p>
+	                 <input class="form-control champ-non-modifiable" name="modele" type="text" value= '<?php print_r($voitures[0][1]);?>'  disabled="disabled">
 	         <p class="help-block text-danger"></p>
                   </div>
 									<div class="form-group" >
-										<label class="control-label col-sm-3" for="couleur">Couleur</label>
+										<p>Couleur</p>
 	                 <input class="form-control champ-non-modifiable" name="couleur" type="text" value= '<?php print_r($voitures[0][2]);?>'  disabled="disabled">
 	         <p class="help-block text-danger"></p>
                   </div>
 									<div class="form-group" >
-										<label class="control-label col-sm-3" for="NbPlaces">Nombre de places</label>
+										<p>Nombre de places</p>
 	                 <input class="form-control champ-non-modifiable" name="NbPlaces" type="text" value= '<?php print_r($voitures[0][3]);?>'  disabled="disabled">
 	         <p class="help-block text-danger"></p>
                   </div>
 									<div class="form-group" >
-										<label class="control-label col-sm-3" for="tailleBagage">Couleur</label>
+										<p>Taille bagage</p>
 	                 <input class="form-control champ-non-modifiable" name="tailleBagage" type="text" value= '<?php print_r($voitures[0][4]);?>'  disabled="disabled">
 	         <p class="help-block text-danger"></p>
                   </div>
--- templates/validateRechercheTrajetTemplate.php
+++ templates/validateRechercheTrajetTemplate.php
@@ -8,7 +8,7 @@
 
 <section class="content-section" id="portfolio">
 
-	<h2 class="section-heading text-uppercase">Trajets recherchés</h2>								
+	<h2 class="section-heading text-uppercase">Trajets recherchés</h2>
 		<table>
 			<caption>Trajets recherchés</caption>
 
@@ -19,33 +19,34 @@
 				<th>Participants</th>
 				<th>Places disponibles</th>
 			</tr>
-			
+
 			<?php
 				$nbTrajets = sizeof($trajetsRecherchés);
 				for ($i=0; $i<$nbTrajets; $i++){ ?>
-			
-			
+
+
 			<tr>
 				<td><?php print_r($trajetsRecherchés[$i]['lieu_depart']);?></td>
 				<td><?php print_r($trajetsRecherchés[$i]['lieu_arrivee']);?></td>
 				<td><?php print_r($trajetsRecherchés[$i]['heure_depart']);?></td>
-		
+
 					<?php
 					// affiche les participants
 					$participants = User::showParticipantsTrajet($trajetsRecherchés[$i]['id_trajet']);
 					$nbParticipants = sizeof($participants);
 					for ($j=0; $j<$nbParticipants; $j++){ ?>
 
-				<td><?php print_r($participants[$j]['nom_user'].' '.$participants[$j]['prenom_user']);?>
-				
+				<td><?php print_r($participants[$j]['nom_user'].' '.$participants[$j]['prenom_user']);?></td>
+
 
 					<?php }
 					?>
+					<td></td>
 				<td><?php print_r($trajetsRecherchés[$i]['nombre_places']-$nbParticipants);?></td>
-				
-				
+
+
 				<td><?php // cache le bouton "m'incrire" si l'utilisateur est déjà participant
-				
+
 					// if(User::showParticipantsTrajet($trajetsRecherchés[$i]['id_trajet'])['id_user'] == $_SESSION['id_user']){
 						// echo("style=\"visibility:hidden\" ");
 					// }
@@ -56,15 +57,15 @@
 						<p><input type="submit" value="M'inscrire" name="boutonInscription" class="btn btn-primary btn-xl text-uppercase"></input></p>
 					</form>
 				</td>
-			</tr>			
-			
+			</tr>
+		<?php } ?>
 		</table>
-					
-					
-					
-					
-					
-					
-	
-	
+
+
+
+
+
+
+
+
 </section>
